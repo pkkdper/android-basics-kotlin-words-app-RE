71a00a0efeb38794916b42010ebb6c4071e190a6
pkkdper <oliwia.giercza@gmail.com>
2023-06-26 21:19:48 -0500

    setting up the recyclew view property

--- android-basics-kotlin-words-app-activities/app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ android-basics-kotlin-words-app-activities/app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -5,10 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.wordsapp.databinding.FragmentLetterListBinding
 
 
 class LetterListFragment : Fragment() {
     private var _binding: FragmentLetterListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.recyclerView
+        chooseLayout()
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLetterListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
