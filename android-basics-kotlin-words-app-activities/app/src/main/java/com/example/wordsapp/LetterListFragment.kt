7c4e166fa176e217a227a521da81ec25f962b384
pkkdper <oliwia.giercza@gmail.com>
2023-06-26 21:04:38 -0500

    beginning of LetterListFragment.kt with declaring binding and making good practices

--- android-basics-kotlin-words-app-activities/app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ android-basics-kotlin-words-app-activities/app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.wordsapp.databinding.FragmentLetterListBinding
 
 
 class LetterListFragment : Fragment() {
-
+    private var _binding: FragmentLetterListBinding? = null
+    private val binding get() = _binding!!
 }
