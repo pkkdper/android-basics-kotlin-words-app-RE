1375be9c1844ca7090ab9df35fcd758c579bae06
pkkdper <oliwia.giercza@gmail.com>
2023-06-30 15:07:39 -0500

    creating onCreateView()

--- android-basics-kotlin-words-app-activities/app/src/main/java/com/example/wordsapp/WordListFragment.kt
+++ android-basics-kotlin-words-app-activities/app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -5,11 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.wordsapp.databinding.FragmentLetterListBinding
 
 
 class WordListFragment : Fragment() {
     companion object {
         const val LETTER = "letter"
         const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+    private var _binding: FragmentLetterListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLetterListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
