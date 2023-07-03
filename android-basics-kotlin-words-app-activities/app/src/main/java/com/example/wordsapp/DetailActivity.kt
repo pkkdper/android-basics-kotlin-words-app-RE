92158c0cf2630ff666edbaedaf000c1041b39982
oliwia <oliwia.giercza@gmail.com>
2023-07-03 14:40:55 -0500

    cleaned out DetailActivity.kt

--- android-basics-kotlin-words-app-activities/app/src/main/java/com/example/wordsapp/DetailActivity.kt
+++ android-basics-kotlin-words-app-activities/app/src/main/java/com/example/wordsapp/DetailActivity.kt
@@ -28,12 +28,7 @@ class DetailActivity : AppCompatActivity() {
      * via DetailActivity.<variable> without needing to create
      * a DetailActivity instance.
      */
-    companion object {
-        const val LETTER = "letter"
-        const val SEARCH_PREFIX = "https://www.google.com/search?q="
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+l    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Retrieve a binding object that allows you to refer to views by id name
@@ -41,21 +36,5 @@ class DetailActivity : AppCompatActivity() {
         // For example, a View with the id word_one is referenced as binding.wordOne
         val binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Retrieve the LETTER from the Intent extras
-        // intent.extras.getString returns String? (String or null)
-        // so toString() guarantees that the value will be a String
-        val letterId = intent?.extras?.getString(LETTER).toString()
-
-        val recyclerView = binding.recyclerView
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = WordAdapter(letterId, this)
-
-        // Adds a [DividerItemDecoration] between items
-        recyclerView.addItemDecoration(
-            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
-        )
-
-        title = getString(R.string.detail_prefix) + " " + letterId
     }
 }
