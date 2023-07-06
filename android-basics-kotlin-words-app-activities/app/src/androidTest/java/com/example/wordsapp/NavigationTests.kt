bd7daee5fb3c434ac28a9063492f06c21533bd04
oliwia <oliwia.giercza@gmail.com>
2023-07-06 14:32:12 -0500

    created a test function and inside test instance of the navigation controller

--- android-basics-kotlin-words-app-activities/app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
+++ android-basics-kotlin-words-app-activities/app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
@@ -1,4 +1,14 @@
 package com.example.wordsapp
 
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Test
+
 class NavigationTests {
+    @Test
+    fun navigate_to_words_nav_component() {
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+    }
 }
