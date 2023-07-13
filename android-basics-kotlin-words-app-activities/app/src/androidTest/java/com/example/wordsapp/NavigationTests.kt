d26ccd8b9c75bf03dae28a0a9334afe1b1b108a5
oliwia <oliwia.giercza@gmail.com>
2023-07-12 19:21:17 -0500

    declared which nav graph to use for the fragment launched

--- android-basics-kotlin-words-app-activities/app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
+++ android-basics-kotlin-words-app-activities/app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
@@ -3,7 +3,12 @@ package com.example.wordsapp
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import org.junit.Test
 
 class NavigationTests {
@@ -20,6 +25,10 @@ class NavigationTests {
 
             Navigation.setViewNavController(fragment.requireView(), navController)
         }
+        onView(withId(R.id.recycler_view))
+            .perform(
+                RecyclerViewActions
+                .actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click()))
     }
 
 }
